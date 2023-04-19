9ff096d35662b015039b9307cbe06250284a4d77
FeliksHelenius <felikshelenius@gmail.com>
2023-04-19 15:21:12 +0200

    Co-authored-by: A Swedish Gamer <ASwedishGamer@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
